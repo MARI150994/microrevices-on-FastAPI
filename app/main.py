6ee7520ab89c42134c0ea28e851d89b2d804261b
MARI150994 <mari.utkina.94@mail.ru>
2022-07-05 12:34:07 +0300

    delete websocket

--- app/main.py
+++ app/main.py
@@ -10,25 +10,11 @@ from app.api.api import api_router
 app = FastAPI()
 app.include_router(api_router)
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-# @app.get("/")
-# async def root():
-#     return FileResponse('static/index.html')
 
 async def init_models():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-
-
-@app.websocket('/ws')
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f'Message text was: {data}')
 
 
 if __name__ == '__main__':
